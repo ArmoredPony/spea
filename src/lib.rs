408f9de04173b127afe8211d0c081618730ac6b1
ArmoredPony <raxmetov2003@gmail.com>
2024-06-01 19:00:13 +0400

    create `Mutator` trait

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,16 @@ impl<T, F: Fn(Vec<T>) -> bool> Terminator<T> for F {
     self(population)
   }
 }
+
+/// Responsible for mutation of solutions in order to maintain genetic
+/// diversity of a population.
+pub trait Mutator<T> {
+  /// Consumes a solution and returns a mutated solution.
+  fn mutate(&mut self, solution: T) -> T;
+}
+
+impl<T, F: Fn(T) -> T> Mutator<T> for F {
+  fn mutate(&mut self, solution: T) -> T {
+    self(solution)
+  }
+}

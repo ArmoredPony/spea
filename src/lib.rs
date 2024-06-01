4a5df366a3214f599b7510a7c98da1e39a510c89
ArmoredPony <raxmetov2003@gmail.com>
2024-06-01 19:59:51 +0400

    rename `Recombinator` to `Breeder

--- src/lib.rs
+++ src/lib.rs
@@ -37,14 +37,14 @@ impl<T, F: Fn(T) -> T> Mutator<T> for F {
 
 /// Responsible for creating offspring solutions by combining existing
 /// solutions.
-pub trait Recombinator<T> {
+pub trait Breeder<T> {
   /// Combines genes of existing solutions and returns a generated set of
   /// solutions.
-  fn combine(&mut self, population: &[T]) -> Vec<T>;
+  fn breed(&mut self, population: &[T]) -> Vec<T>;
 }
 
-impl<T, F: Fn(&[T]) -> Vec<T>> Recombinator<T> for F {
-  fn combine(&mut self, population: &[T]) -> Vec<T> {
+impl<T, F: Fn(&[T]) -> Vec<T>> Breeder<T> for F {
+  fn breed(&mut self, population: &[T]) -> Vec<T> {
     self(population)
   }
 }

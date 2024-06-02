22b932b169ceed1e072130bd75b94eb490d48d83
ArmoredPony <raxmetov2003@gmail.com>
2024-06-02 19:48:03 +0400

    implement pareto dominance

--- src/objective.rs
+++ src/objective.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 /// Represents an objective that solutions should converge on.
 pub trait Objective<T> {
   /// Tests how close is current solution to the goal.
@@ -7,5 +9,27 @@ pub trait Objective<T> {
 impl<T, F: Fn(&T) -> f32> Objective<T> for F {
   fn test(&self, solution: &T) -> f32 {
     self(solution)
+  }
+}
+
+struct Objectives<T, const N: usize>([Box<dyn Objective<T>>; N]);
+
+trait ParetoDominance {
+  fn dominance_ord(&self, other: &Self) -> Ordering;
+}
+
+impl<const N: usize> ParetoDominance for [f32; N] {
+  fn dominance_ord(&self, other: &Self) -> Ordering {
+    let mut ord = Ordering::Equal;
+    for (s, o) in self.iter().zip(other) {
+      let ord_i = s.partial_cmp(o).expect("attempted to compare a NaN");
+      match (ord_i, ord) {
+        (Ordering::Greater, Ordering::Less)
+        | (Ordering::Less, Ordering::Greater) => return Ordering::Equal,
+        (_, Ordering::Equal) => ord = ord_i,
+        _ => (),
+      }
+    }
+    ord
   }
 }

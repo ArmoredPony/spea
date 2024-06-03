bd5117dd2ed0854b31e727204f09986a8006bc34
ArmoredPony <raxmetov2003@gmail.com>
2024-06-03 16:05:53 +0400

    update `mutate` signature
    sacrifice ability to change mutator`s state for sake of parallelism

--- src/mutator.rs
+++ src/mutator.rs
@@ -2,11 +2,11 @@
 /// diversity of a population.
 pub trait Mutator<T> {
   /// Consumes a solution and returns a mutated solution.
-  fn mutate(&mut self, solution: &mut T);
+  fn mutate(&self, solution: &mut T);
 }
 
-impl<T, F: Fn(&mut T)> Mutator<T> for F {
-  fn mutate(&mut self, solution: &mut T) {
+impl<T, F: Fn(&T)> Mutator<T> for F {
+  fn mutate(&self, solution: &mut T) {
     self(solution)
   }
 }

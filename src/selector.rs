499169b05780c0e507495c0561ce0b9e6f72f6ad
ArmoredPony <raxmetov2003@gmail.com>
2024-06-03 15:59:26 +0400

    update `select` signature

--- src/selector.rs
+++ src/selector.rs
@@ -2,11 +2,11 @@
 pub trait Selector<T> {
   /// Selects solutions from given population to procreate the next generation
   /// from.
-  fn select(&mut self, population: Vec<T>) -> Vec<T>;
+  fn select<'a>(&'a mut self, population: &'a [T]) -> &'a [T];
 }
 
-impl<T, F: Fn(Vec<T>) -> Vec<T>> Selector<T> for F {
-  fn select(&mut self, population: Vec<T>) -> Vec<T> {
+impl<T, F: Fn(&[T]) -> &[T]> Selector<T> for F {
+  fn select<'a>(&'a mut self, population: &'a [T]) -> &[T] {
     self(population)
   }
 }

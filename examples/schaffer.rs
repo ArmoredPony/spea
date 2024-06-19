4e937fd2edd9273348cac190bc7828de176d9980
ArmoredPony <raxmetov2003@gmail.com>
2024-06-19 13:59:54 +0400

    add schaffer n1 function example

--- examples/schaffer.rs
+++ examples/schaffer.rs
@@ -1,55 +1,59 @@
-use rand::{self, Rng};
-use spea::{
-  breeder::{BinaryCrossover, Breeder},
-  terminator::Generations,
-  Spea2,
-};
+use itertools::Itertools;
+use rand::{self, seq::SliceRandom, Rng};
+use rand_distr::Distribution;
+use spea::Spea2;
 
 fn main() {
-  fn crossover<'a>(a: &'a f32, b: &'a f32) -> f32 {
-    let r = rand::thread_rng().gen_range(-0.25..1.25);
-    a * r + b * (1.0 - r)
+  let mut generations = 1000;
+  let terminator = |_: &[&f32]| {
+    generations -= 1;
+    generations == 0
+  };
+
+  fn selector<'a>(v: &'a [&'a f32]) -> Vec<&'a f32> {
+    v.choose_multiple(&mut rand::thread_rng(), 10)
+      .copied()
+      .collect()
   }
 
-  fn selector<'a>(v: &'a [(&'a f32, f32)]) -> Vec<&'a f32> {
-    v.iter().take(10).map(|(s, _)| *s).collect()
-  }
+  let crossover = |population: &[&f32]| {
+    population
+      .iter()
+      .cartesian_product(population)
+      .map(|(a, b)| {
+        let r = rand::thread_rng().gen_range(-0.25..1.25);
+        *a * r + *b * (1.0 - r)
+      })
+      .collect()
+  };
 
-  let objectives = [|x: &f32| x.powf(2.0), |x: &f32| (x - 2.0).powf(2.0)];
+  let norm_dist = rand_distr::Normal::new(0.0f32, 200.0 / 6.0).unwrap();
+  let mutator = |v: &mut f32| {
+    if rand::thread_rng().gen::<f32>() <= 0.05 {
+      *v += norm_dist.sample(&mut rand::thread_rng())
+    }
+  };
 
-  let initial_population = (0..10).map(|v| v as f32).collect::<Vec<_>>();
-  let archive_size = initial_population.len();
+  let objectives = [
+    |x: &f32| x.powf(2.0), //
+    |x: &f32| (x - 2.0).powf(2.0),
+  ];
 
-  let spea2 = Spea2::new(
+  let initial_population = (-100..100).map(|v| v as f32).collect::<Vec<_>>();
+  let archive_size = 100;
+
+  let mut spea2 = Spea2::new(
     initial_population,
     archive_size,
-    Generations::new(100),
+    terminator,
     selector,
-    // FIXME: it should be replaced with a closure
-    Crossover {},
-    |_: &_| (),
+    crossover,
+    mutator,
     objectives.into(),
   );
 
-  let results = spea2.run();
-  println!("{:?}", results);
-}
-
-struct Crossover;
-
-impl Breeder<f32> for Crossover {
-  fn breed(&self, population: &[&f32]) -> Vec<f32> {
-    let p = population[..population.len() - 1]
-      .iter()
-      .enumerate()
-      .flat_map(move |(i, a)| {
-        population[i..].iter().map(|b| {
-          let r = rand::thread_rng().gen_range(-0.25..1.25);
-          *a * r + *b * (1.0 - r)
-        })
-      })
-      .collect::<Vec<_>>();
-    println!("solutions after breeding: {}", p.len());
-    p
-  }
+  spea2.run();
+  let mut results = spea2.get_nondominated_solutions();
+  results.sort_unstable_by(|a, b| a.total_cmp(b));
+  println!("{:?}: {}", results, results.len());
 }
